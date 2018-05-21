3946a6bbac5a83358b03c82b85d0dd9e0e155620
henry j. calani A <hjcalani@gmail.com>
2018-05-21 04:42:16 -0400

    upload the resource for model

--- src/main/java/com/ssi/ssi/resources/MaterialResource.java
+++ src/main/java/com/ssi/ssi/resources/MaterialResource.java
@@ -1,4 +1,45 @@
 package com.ssi.ssi.resources;
 
+import com.ssi.ssi.domain.model.Material;
+import com.ssi.ssi.domain.model.MaterialType;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class MaterialResource {
+    private final Material material;
+    private Long id;
+    private String name;
+    private MaterialType materialType;
+
+    public  MaterialResource(final Material material){
+        this.material = material;
+        this.id = material.getId();
+        this.name = material.getName();
+        this.materialType = material.getMaterialType();
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MaterialType getMaterialType() {
+        return materialType;
+    }
+
+    public void setMaterialType(MaterialType materialType) {
+        this.materialType = materialType;
+    }
 }
--- src/main/java/com/ssi/ssi/resources/StoreResource.java
+++ src/main/java/com/ssi/ssi/resources/StoreResource.java
@@ -1,4 +1,51 @@
 package com.ssi.ssi.resources;
 
+import com.ssi.ssi.domain.model.Material;
+import com.ssi.ssi.domain.model.Store;
+
 public class StoreResource {
+    private  final Store store;
+    private Long id;
+    private String name;
+    private Material material;
+    private Integer quantity;
+
+    public  StoreResource(final Store store){
+        this.store = store;
+        this.id = store.getId();
+        this.name = store.getName();
+        this.quantity = store.getQuantity();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
