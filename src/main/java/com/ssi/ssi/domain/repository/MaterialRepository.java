44f081c482be37b5205037f087657fa804ad733b
lee-ivanov <lee.19.ra@gmail.com>
2018-05-21 22:40:24 -0400

    editing material clases

--- src/main/java/com/ssi/ssi/domain/repository/MaterialRepository.java
+++ src/main/java/com/ssi/ssi/domain/repository/MaterialRepository.java
@@ -1,4 +1,7 @@
 package com.ssi.ssi.domain.repository;
 
-public interface MaterialRepository {
+import com.ssi.ssi.domain.model.Material;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MaterialRepository extends CrudRepository<Material, Long>{
 }
