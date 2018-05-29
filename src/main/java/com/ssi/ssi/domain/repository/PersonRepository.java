1f88eebc4198381eee58ecb9ebe0af4925d2c16b
alvaroVR <alvaro.10.vr@gmail.com>
2018-05-28 21:25:50 -0400

    adde method to search person

--- src/main/java/com/ssi/ssi/domain/repository/PersonRepository.java
+++ src/main/java/com/ssi/ssi/domain/repository/PersonRepository.java
@@ -1,7 +1,16 @@
 package com.ssi.ssi.domain.repository;
 
 import com.ssi.ssi.domain.model.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+
+    @Query("SELECT p FROM Person p WHERE p.firstName like :search1 OR p.lastName like :search2 OR p.ci like :search3")
+    public List<Person> findByFirstNameLikeAndCiLike(@Param("search1") String search1, @Param("search2") String search2, @Param("search3") String search3);
 }
