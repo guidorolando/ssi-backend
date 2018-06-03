0c39d9858cc1294260121dfc50eef1c77eb998c9
Marco Herrera“ <antony.roca12@gmail.com>
2018-06-03 11:38:38 -0400

    remove responsability

--- src/main/java/com/ssi/ssi/domain/repository/StoreRepository.java
+++ src/main/java/com/ssi/ssi/domain/repository/StoreRepository.java
@@ -5,11 +5,12 @@ import com.ssi.ssi.domain.model.Store;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface StoreRepository extends CrudRepository<Store, Long> {
 
-   @Query("select store from Store store where mat")
-    List<Material> searchByText(@Param("id") Integer id);
+   /*@Query("select store from Store store where mat")
+    List<Material> searchByText(@Param("id") Integer id);*/
 }
