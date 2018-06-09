e4f96012261d5e5bfa7b5bcf2012d5145ee5de98
LICE <liceliarojasc@gmail.com>
2018-06-09 14:59:55 -0400

    report incident

--- src/main/java/com/ssi/ssi/domain/repository/AssignmentRepository.java
+++ src/main/java/com/ssi/ssi/domain/repository/AssignmentRepository.java
@@ -2,7 +2,9 @@ package com.ssi.ssi.domain.repository;
 
 import com.ssi.ssi.domain.model.Assignment;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AssignmentRepository extends CrudRepository<Assignment, Long> {
 
 }
