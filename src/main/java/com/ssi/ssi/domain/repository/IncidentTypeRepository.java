b03940a517927449f3dd7c7fde4a08cb27e44bc8
guido <guidorolando91@gmail.com>
2018-06-23 21:15:47 -0400

    procedure

--- src/main/java/com/ssi/ssi/domain/repository/IncidentTypeRepository.java
+++ src/main/java/com/ssi/ssi/domain/repository/IncidentTypeRepository.java
@@ -2,6 +2,7 @@ package com.ssi.ssi.domain.repository;
 
 import com.ssi.ssi.domain.model.IncidentType;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,4 +11,8 @@ public interface IncidentTypeRepository extends CrudRepository<IncidentType, Lon
 
     @Query("SELECT incidentType FROM IncidentType incidentType WHERE incidentType.isDeleted=0 ")
     List<IncidentType> findAll();
+
+
+    @Procedure
+    void createIncidentType(String name, String description);
 }
